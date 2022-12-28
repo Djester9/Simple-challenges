09ff96774b227623265cb18fecddf7d420ec220c
Connor Jacob <116337944+Djester9@users.noreply.github.com>
2022-12-28 12:49:51 +0800

    Update sum of even numbers in a range.py

--- sum of even numbers in a range.py
+++ sum of even numbers in a range.py
@@ -6,7 +6,7 @@ num = input('which num: ') #allow user to pick number
 sum = 0 #define varaible as 'sum' which = 0
 
 num = (int(num))
-num += 1
+num += 1 #adding one number to users num to make more user friendly. (as range only goes upto to but does not include picked num 
 
 for e in range(1, num):
     if e % 2 == 0:
